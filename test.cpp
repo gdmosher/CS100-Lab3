619f3d82d28a8feb3e593161d4f7bdbbddcf0a4c
Yichao Chen <ychen177@hammer.cs.ucr.edu>
2017-10-26 15:03:55 -0700

    Added even more test cases in test.cpp

--- test.cpp
+++ test.cpp
@@ -14,18 +14,19 @@ int main() {
     Op* op4 = new Op(6);
 /*  Extend test cases here */
 //    Op* op5 = new Op(0);
-    Op* op6 = new Op(-1.5);
-//    Op* op7 = new Op(1e-2);
+    Op* op6 = new Op(-1.4);
+    Op* op7 = new Op(1e-2);
     Mult* A = new Mult(op1, op2);
     Sub* B = new Sub(op3, op4);
     Add* C = new Add(A, B);
     Sqr* D = new Sqr(C);
 /*  Extend test cases here */
     Div* E = new Div(D, op2);
-    Mult* F = new Mult(E, op6);
-    Ceil* G = new Ceil(F);
-    Abs* H = new Abs(G);
-    Floor* I = new Floor(H);
+    Mult* F = new Mult(E, op6); //-44.8
+    Ceil* G = new Ceil(F);      //-44
+    Abs* H = new Abs(G);        //44
+    Mult* I = new Mult(H, op7); //.44
+    Floor* J = new Floor(I);    //0
 
     VectorContainer* v_container = new VectorContainer();
     v_container->add_element(A);
@@ -36,6 +37,7 @@ int main() {
     v_container->add_element(G);
     v_container->add_element(H);
     v_container->add_element(I);
+    v_container->add_element(J);    
     cout << "Container Before Sort: " << endl;
     v_container->print();
     cout << endl;
@@ -54,11 +56,13 @@ int main() {
     l_container->add_element(G);
     l_container->add_element(H);
     l_container->add_element(I);
+    l_container->add_element(J);
     cout << "Container Before Sort: " << endl;
     l_container->print();
     cout << endl;
 //    l_container->set_sort_function(new SelectionSort());
 //    l_container->set_sort_function(new BubbleSort());
+    cout << "\nIntentionally generate set_sort_function() here.";
     l_container->sort();	// call sort without calling set_sort_function()
     l_container->set_sort_function(new BubbleSort());
     l_container->sort();	
